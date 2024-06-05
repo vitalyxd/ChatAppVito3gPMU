536249cf189fe1491509f3f337389cfb5c048882
Sch1me <95961952+Sch1me@users.noreply.github.com>
2024-06-05 08:21:51 +0200

    TODO auth popravit

--- app/src/main/java/com/vito/chatappvito3gpmu/MainActivity.kt
+++ app/src/main/java/com/vito/chatappvito3gpmu/MainActivity.kt
@@ -1,8 +1,10 @@
-package com.vito.chatappvito3gpmu
-
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -16,11 +18,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
     private val porukeList = mutableListOf<Poruka>()
     private lateinit var adapter: PorukaAdapter
+    private val register = Register()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root) 
+        setContentView(binding.root)
 
         val databaseUrl = "https://chatappvito3gpmu-default-rtdb.europe-west1.firebasedatabase.app/"
         database = Firebase.database(databaseUrl).reference
@@ -36,6 +39,24 @@ class MainActivity : AppCompatActivity() {
             if (messageText.isNotEmpty() && senderText.isNotEmpty()) {
                 val message = Poruka(messageText, senderText)
                 sendMessageToFirebase(message)
+            }
+        }
+
+        binding.registerButton.setOnClickListener {
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                register.registerUser(email, password) { success, errorMessage ->
+                    if (success) {
+                        Toast.makeText(this, "Registracija uspješna", Toast.LENGTH_SHORT).show()
+                        // todo nakon registracije
+                    } else {
+                        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Molimo unesite e-mail i lozinku", Toast.LENGTH_SHORT).show()
             }
         }
 
